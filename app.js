5ae42085b3edef05fc47f5f27acd0668bcb82568
rahulbabu7 <rahulbabu436@gmail.com>
2023-05-26 20:02:02 +0530

    updated

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
 class Book {
   constructor(title, author, isbn) {
-    this.title= title;
+    this.title = title;
     this.author = author;
     this.isbn = isbn;
   }
 }
 
 class Ui {
-    addBookToList(book)
-    {
-      const list  = document.getElementById('book-list');  //table body
-      //create tr element
-      const row = document.createElement('tr');
-      row.innerHTML = `
+  addBookToList(book) {
+    const list = document.getElementById("book-list"); //table body
+    //create tr element
+    const row = document.createElement("tr");
+    row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.isbn}</td>
       <td><a href= "#" class= "delete">X</a></td>`;
 
-      list.appendChild(row);
+    list.appendChild(row);
+  }
+  showAlert(message, className) {
+    //create a div
+    const div = document.createElement("div");
+    //add class
+    div.className = `alert ${className}`;
+    //add text
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector(".container");
+    //get form
+    const form = document.querySelector("#book_form");
+    //insert alert
+    container.insertBefore(div, form);
 
+    //timeout after  sec
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
+  deleteBook(target) {
+    if (target.className === "delete") {
+      target.parentElement.parentElement.remove();
     }
-    showAlert(message,className){
-      //create a div
-      const div =  document.createElement('div');
-      //add class
-      div.className = `alert ${className}`;
-      //add text
-      div.appendChild(document.createTextNode(message));
-      //get parent
-      const container = document.querySelector('.container');
-      //get form
-      const form = document.querySelector('#book_form');
-      //insert alert
-      container.insertBefore(div,form);
-
-      //timeout after  sec
-      setTimeout(()=>{
-        document.querySelector('.alert').remove();
-      },3000);
-
-    }
-    deleteBook(target)
-    {
-
-    }
-    clearFields()
-    {
-
-    }
+  }
+  clearFields() {
+    document.getElementById('title').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('isbn').value = "";
+  }
 }

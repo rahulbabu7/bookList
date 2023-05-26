26e116661699cd40cba8d232cad800abf603dbf0
rahulbabu7 <rahulbabu436@gmail.com>
2023-05-26 19:31:25 +0530

    added showalert and add book details

---
+++ app.js
@@ -0,0 +1,52 @@
+class Book {
+  constructor(title, author, isbn) {
+    this.title= title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+class Ui {
+    addBookToList(book)
+    {
+      const list  = document.getElementById('book-list');  //table body
+      //create tr element
+      const row = document.createElement('tr');
+      row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href= "#" class= "delete">X</a></td>`;
+
+      list.appendChild(row);
+
+    }
+    showAlert(message,className){
+      //create a div
+      const div =  document.createElement('div');
+      //add class
+      div.className = `alert ${className}`;
+      //add text
+      div.appendChild(document.createTextNode(message));
+      //get parent
+      const container = document.querySelector('.container');
+      //get form
+      const form = document.querySelector('#book_form');
+      //insert alert
+      container.insertBefore(div,form);
+
+      //timeout after  sec
+      setTimeout(()=>{
+        document.querySelector('.alert').remove();
+      },3000);
+
+    }
+    deleteBook(target)
+    {
+
+    }
+    clearFields()
+    {
+
+    }
+}
